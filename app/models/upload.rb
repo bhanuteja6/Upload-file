908ce68a43c4e42e725bf5f3e4618c4bb51bbd19
bhanuteja6 <bhanuteja6@gmail.com>
2013-01-16 18:39:34 +0530

    removed style

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,11 +1,5 @@
 class Upload < ActiveRecord::Base
   attr_accessible :document, :email, :name, :photo
   
-  has_attached_file :photo,
-    :styles => {
-      :thumb=> "100x100",
-      :small  => "400x400>" ,
-      :path => ":rails_root/public/system/upload",
-      :url => "/system/upload/" }
-       
+  has_attached_file :photo     
 end
